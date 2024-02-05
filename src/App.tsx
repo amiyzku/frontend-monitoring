3f505a5539623942651b587c6efe7850ff1631d9
Yuma Miyazaki <yuma.miyazaki@satori.team>
2024-02-05 20:46:33 +0900

    feat: Search postal code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-function App() {
+import { useState } from 'react';
+
+const App = () => {
+  const [postalCode, setPostalCode] = useState('');
+  const [result, setResult] = useState('');
+
+  const clickHandler = async () => {
+    setResult('');
+    const baseUrl = 'https://zipcloud.ibsnet.co.jp/api/search';
+    const url = `${baseUrl}?zipcode=${postalCode}`;
+    await fetch(url, { method: 'GET' }).then(async res => {
+      let text = await res.text();
+      setResult(text);
+    });
+  };
+
   return (
     <>
-      <div>App</div>
+      <div>Search Postal Code</div>
+      <input
+        value={postalCode}
+        onChange={event => setPostalCode(event.target.value)}
+      />
+      <button onClick={clickHandler}>検索</button>
+
+      <pre>{result}</pre>
     </>
   );
-}
+};
 
 export default App;
